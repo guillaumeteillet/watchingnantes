6cc4546e119d7fbeb2e7288ab9a234f4acd0612f
Guillaume Teillet <guillaume@kaemo.com>
2016-02-08 13:09:55 +0100

    A basic twitter bot who upload images.

---
+++ download.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import requests
+
+def download_image(filename, url):
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for data in request:
+                image.write(data)
+        return 200
+    else:
+        return 404
